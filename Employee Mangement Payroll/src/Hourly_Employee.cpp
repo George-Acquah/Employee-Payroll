94208541b35bcd3a09085fbb633636b476fed02d
George-Acquah <georgeacquah889@gmail.com>
2023-03-21 17:07:18 +0000

    defined member functions for hourly employees

--- Employee Mangement Payroll/src/Hourly_Employee.cpp
+++ Employee Mangement Payroll/src/Hourly_Employee.cpp
@@ -4,3 +4,35 @@ Hourly_Employee::Hourly_Employee()
 {
     //ctor
 }
+
+
+void Hourly_Employee::title()
+{
+    cout<<"WELCOME TO THE HOURLY EMPLOYEE PORTAL"<<endl;
+    cout<<"^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^"<<endl;
+}
+
+void Hourly_Employee::getSalary()
+{
+    cout<<endl<<"Please enter your employee number: ";
+    cin>>Emp_Num;
+    cout<<"Please enter your name here: ";
+    cin>>Emp_Name;
+    cout<<"Please enter your hourly wage here: ";
+    cin>>hourly_wage;
+    cout<<"Please enter the number of hours you worked: ";
+    cin>>hours_worked;
+    cout<<endl;
+}
+
+void Hourly_Employee::display()
+{
+    Salary=hourly_wage*hours_worked;
+    cout<<"This is the details of "<<Emp_Name<<endl;
+    cout<<"~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"<<endl<<endl;
+    cout<<"The employee number is "<<Emp_Num<<endl;
+    cout<<"The employee name is "<<Emp_Name<<endl;
+    cout<<Emp_Name<<"'s hourly wage is : "<<hourly_wage<<endl;
+    cout<<Emp_Name<<" worked for "<<hours_worked<<"hours"<<endl;
+    cout<<"Therefore "<<Emp_Name<<"'s salary is : "<<Salary<<endl;
+}
