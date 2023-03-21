8f0f0f7586b10c5ba8d59d4ea25af2d20eb8cef4
George-Acquah <georgeacquah889@gmail.com>
2023-03-21 16:45:31 +0000

    Created data members and member functions for Employee

--- Employee Mangement Payroll/include/Employee.h
+++ Employee Mangement Payroll/include/Employee.h
@@ -6,8 +6,13 @@ class Employee
 {
     public:
         Employee();
+        virtual void getSalary()=0;
+        virtual void display()=0;
 
     protected:
+        int Emp_Num;
+        char Emp_Name[30];
+        float Salary;
 
     private:
 };
