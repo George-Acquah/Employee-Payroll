868251a9001bf97d98209963827014050ff7c1f1
George-Acquah <georgeacquah889@gmail.com>
2023-03-21 16:48:10 +0000

    created data members and member functions for commissioned employee

--- Employee Mangement Payroll/include/Commissioned_Employee.h
+++ Employee Mangement Payroll/include/Commissioned_Employee.h
@@ -9,9 +9,14 @@ class Commissioned_Employee : public Employee
     public:
         Commissioned_Employee();
 
+        void getSalary();
+        void display();
+        void title();
+
     protected:
 
     private:
+        int overtime;
 };
 
 #endif // COMMISSIONED_EMPLOYEE_H
