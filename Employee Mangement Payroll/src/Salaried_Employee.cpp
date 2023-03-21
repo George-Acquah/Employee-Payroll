b14b33ca5d8bc1d5fa2241a2941dbf0a69f8009e
George-Acquah <georgeacquah889@gmail.com>
2023-03-21 17:05:07 +0000

    defined member functions for salaried employees

--- Employee Mangement Payroll/src/Salaried_Employee.cpp
+++ Employee Mangement Payroll/src/Salaried_Employee.cpp
@@ -4,3 +4,30 @@ Salaried_Employee::Salaried_Employee()
 {
     //ctor
 }
+
+
+
+void Salaried_Employee::title()
+{
+    cout<<"WELCOME TO THE SALARIED EMPLOYEE PORTAL"<<endl;
+    cout<<"^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^"<<endl;
+}
+
+void Salaried_Employee::getSalary()
+{
+    cout<<endl<<"Please enter your employee number: ";
+    cin>>Emp_Num;
+    cout<<"Please enter your name here: ";
+    cin>>Emp_Name;
+    cout<<"Please enter your salary here: ";
+    cin>>Salary;
+    cout<<endl;
+}
+
+void Salaried_Employee::display()
+{
+    cout<<"This is the details of "<<Emp_Name<<endl;
+    cout<<"The employee name is "<<Emp_Name<<endl;
+    cout<<"The employee number is "<<Emp_Num<<endl;
+    cout<<Emp_Name<<"'s salary is : "<<Salary<<endl;
+}
