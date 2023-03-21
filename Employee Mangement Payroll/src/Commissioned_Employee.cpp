9ffc5b1f56a3118087e23ae0177d825604cfbb36
George-Acquah <georgeacquah889@gmail.com>
2023-03-21 16:55:27 +0000

    defined member functions for commissioned employees

--- Employee Mangement Payroll/src/Commissioned_Employee.cpp
+++ Employee Mangement Payroll/src/Commissioned_Employee.cpp
@@ -4,3 +4,32 @@ Commissioned_Employee::Commissioned_Employee()
 {
     //ctor
 }
+
+void Commissioned_Employee::title()
+{
+    cout<<"WELCOME TO THE COMMISIONED EMPLOYEE PORTAL"<<endl;
+    cout<<"^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^"<<endl;
+}
+
+void Commissioned_Employee::getSalary()
+{
+    cout<<endl<<"Please enter your employee number: ";
+    cin>>Emp_Num;
+    cout<<"Please enter your name here: ";
+    cin>>Emp_Name;
+    cout<<"Please enter your base salary here: ";
+    cin>>Salary;
+    cout<<"Please enter the overtime hours here: ";
+    cin>>overtime;
+    cout<<endl;
+}
+
+void Commissioned_Employee::display()
+{
+    Salary = Salary+(overtime*0.25);
+    cout<<"This is the details of "<<Emp_Name<<endl;
+    cout<<"The employee number is "<<Emp_Num<<endl;
+    cout<<"The employee name is "<<Emp_Name<<endl;
+    cout<<Emp_Name<<"'s overtime hours is : "<<overtime<<endl;
+    cout<<Emp_Name<<"'s salary is : "<<Salary<<endl;
+}
